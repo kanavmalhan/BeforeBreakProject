52f21c275927bfad8d523296093558f1836693bb
Kanav Malhan <kanavmalhan1@gmail.com>
2025-12-17 17:50:33 -0500

    Starts the init function in the game class which makes every square on the board an empty location.

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,15 @@
-import java.util.ArrayList;
 
 public class Game {
-    private ArrayList<Location> board;
+    private Location[][] board;
     private TimeOfDay time;
+
+    public void init(){
+        //Initializes the entire board to be empty locations so that I can go in and create specific locations later in the function
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                board[i][j] = new Location(i, j, "Nothing");
+            }
+        }
+    }
+
 }
